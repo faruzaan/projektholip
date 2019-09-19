799f0372a6c2015aa197cbf8d2783d7d5bb42b4e
Farhan <farhan.ff.fauzaan@gmail.com>
2019-09-20 02:37:03 +0700

    baru, semangat menjalani hidup teman

--- app/src/main/java/com/example/perpustakaansmk/MainActivity.java
+++ app/src/main/java/com/example/perpustakaansmk/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.perpustakaansmk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static int SPLASH_TIME = 3000;
+    SessionManager sessionManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sessionManager  = new SessionManager(getApplicationContext());
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run()
+            {
+                Intent i;
+                if (sessionManager.is_login())
+                {
+                    i = new Intent(MainActivity.this,MenuActivity.class);
+                }
+                else {
+                    i = new Intent(MainActivity.this, LoginActivity.class);
+                }
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_TIME);
     }
 }
